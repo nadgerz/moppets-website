24301630243cceb2f44409f77852809288241316
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-30 17:20:05 +0200

    positioned <main>, fixed layout bug in header

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-// import "./layout.css"
+
 import "../assets/css/layout.scss"
 import "../assets/css/base/resets.scss"
 import "../assets/css/base/typography.scss"
@@ -31,19 +31,12 @@ const Layout = ({ children }) => (
     render={data => (
       <div className={'site-container'}>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
+        
+          <main id={'main'}>{children}</main>
+        
           <footer>
             © {new Date().getFullYear()} {data.site.siteMetadata.author}
           </footer>
-        </div>
       </div>
     )}
   />
