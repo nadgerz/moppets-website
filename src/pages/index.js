30b23a9979771077113d53be2c9c4f6e019103dd
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-30 19:11:45 +0200

    added BannerRegion to Index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
+import BannerRegion from "../components/bannerRegion"
+
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h3>Hi Sheeple</h3>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/contact/">Go to contact page</Link>
-    <br />
-    <Link to="/about/">Go to about me page</Link>
+    <SEO title="Home"/>
+      <main>
+          <h3>Hi Sheeple</h3>
+          <p>Now go build something great.</p>
+          <BannerRegion />
+      </main>
   </Layout>
 )
 
