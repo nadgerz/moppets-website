d81c8e1b2aaabded874b4ea555dd78a5907894c8
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-30 19:17:46 +0200

    added styling to banner-region

--- src/components/bannerRegion.js
+++ src/components/bannerRegion.js
@@ -3,26 +3,29 @@ import React from "react"
 
 import "../assets/css/components/banner-region.scss"
 
-const BannerRegion = ({ img }) => (
 
-    <div className="banner-region">
+const BannerRegion = ( { img } ) => (
   
-      <aside className="sidebar-left" />
-  
-      <div className="container-two-thirds-width">
-        <p className="text-copy">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-          laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quat. luptatum zzril
-          delenit
-          augue duis dolore te feugait nulla facilisi.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-          ut
-          laoreet dolore magna aliquam erat volutpat.
-        </p>
-      </div>
+  <div className="banner-region">
+    
+    <aside className="sidebar-left">
+      <svg className={".svg-hiBubble "}/>
+    </aside>
+    
+    <div className="container-two-thirds-width">
+      <p className="text-copy">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+        laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quat. luptatum zzril
+        delenit
+        augue duis dolore te feugait nulla facilisi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+        ut
+        laoreet dolore magna aliquam erat volutpat.
+      </p>
     </div>
+  </div>
 )
 
 BannerRegion.propTypes = {
