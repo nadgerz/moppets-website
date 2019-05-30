c09feb8bf452c03c32b76183a86f7f3b1a5a97f2
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-30 17:50:23 +0200

    imported Menu comp to Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,14 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Menu from "./menu.js"
 
 const Header = ({ siteTitle }) => (
   <header>
     <div className="header-content">
       <div className={"nav-branding sidebar-left"}>
         <h1>{siteTitle}</h1>
-          <Link
-            to="/">
-            {siteTitle}
-          </Link>
+        <Menu />
       </div>
       
       <div className="container-full-width header-right" />
