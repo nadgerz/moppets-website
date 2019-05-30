543c32bd07fd09548b5130fc8522b1c3c2c3f5d9
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-30 19:34:48 +0200

    created Skill.js and skill.scss

---
+++ src/components/skill.js
@@ -0,0 +1,56 @@
+// import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+import "../assets/css/components/skill.scss"
+
+const Skills = () => (
+  <div className={'skills'}>
+    
+    <!-- T E C H -->
+    <section className="tech-stack skill">
+      <aside className="sidebar-left">
+        <h3 className="tech">Tech</h3>
+      </aside>
+      <div className="container-full-width">
+        <ul>
+          <li>React JS</li>
+          <li>JS</li>
+          <li>SASS</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>GIT</li>
+        </ul>
+      </div>
+    </section>
+  
+    <!-- S O F T W A R E -->
+    <section className="software-stack skill">
+      <aside className="sidebar-left">
+        <h3 className="software">Software</h3>
+      </aside>
+      <div className="container-full-width">
+        <ul>
+          <li>Jetbrains Webstorm IDE</li>
+          <li>Sublime Text</li>
+          <li>Jira</li>
+          <li>Adobe XD</li>
+          <li>Illustrator</li>
+          <li>InDesign</li>
+          <li>Photoshop</li>
+        </ul>
+      </div>
+    </section>
+    
+  </div>
+)
+
+// Skills.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+//
+// Skills.defaultProps = {
+//   siteTitle: `my site`,
+// }
+
+export default Header
