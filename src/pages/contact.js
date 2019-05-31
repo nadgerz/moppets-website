80f24af6f42f72b7561c1f9cfd5706099cb61f59
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-31 14:40:06 +0200

    added contact.sccs, importet into Contact comp

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,12 +3,22 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../assets/css/components/contact.scss"
+
 
 const Contact = () => (
   <Layout>
-    <SEO title="Contact me" />
-    <h3>Hi from the contact page</h3>
-    <p>Welcome to contact</p>
+    <SEO title="Contact me"/>
+    <div className={'contact'} id={'contact'}>
+      
+      <aside className={"sidebar-left"}/>
+      
+      <div>
+        <h2>Hi from the contact page</h2>
+        <p>Welcome to contact</p>
+      </div>
+    
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
