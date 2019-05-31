c0a89b4bd94cae352d33febb61a7f2d7a1eb804f
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-31 16:18:39 +0200

    added about.scss to css/pages, imported into About comp

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,13 +3,18 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../assets/css/pages/about.scss"
+
 
 const About = () => (
   <Layout>
-    <SEO title="About Me" />
-    <h1>Hi from the About Me page</h1>
-    <p>Welcome to About</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="about" id="about">
+      
+      <SEO title="About Me"/>
+      <h2>Hi from the About Me page</h2>
+      <p>Welcome to About</p>
+    
+    </div>
   </Layout>
 )
 
