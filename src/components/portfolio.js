9332fc613dc0db0eb2591f5b2a5ccbeb1d69c86c
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-05-31 09:10:52 +0200

    created portfolio.js and .scss

---
+++ src/components/portfolio.js
@@ -0,0 +1,36 @@
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+import React from "react"
+
+import "../assets/css/components/portfolio.scss"
+
+const Portfolio = ({  }) => (
+  <section className="recent-work">
+    <aside className="sidebar-left">
+      <h2 className="work">Recent Work</h2>
+    </aside>
+    
+    <div className="container">
+      <div className="container-half-width">
+        <ul className="clients">
+          <li><a href="https://www.neumann-hug.com">Neumann Hug Collection</a></li>
+          <li><a href="https://www.moerbeck.de">Kevin van Moerbeck</a></li>
+        </ul>
+      </div>
+      
+      <div className="container-half-width download">
+        <img src="/branding/imgs/portfolio.png" alt="download my portfolio" />
+      </div>
+    </div>
+  </section>
+)
+
+// Portfolio.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+//
+// Portfolio.defaultProps = {
+//   siteTitle: `my site`,
+// }
+
+export default Portfolio
