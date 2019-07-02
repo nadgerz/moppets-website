8d2b8741098fea07d8adb09c9178fd8af6339f4f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-07-02 21:08:21 +0200

    changed footer & header height

--- src/components/skills.js
+++ src/components/skills.js
@@ -4,15 +4,16 @@ import React from "react"
 
 import "../assets/css/components/skills.scss"
 
+
 const Skills = () => (
-  <div className={'skills'}>
+  <div className={"skills"}>
     {/* //  T E C H */}
     <section className="skill">
       <aside className="sidebar-left">
         <h3>Tech</h3>
       </aside>
       <div className="container-full-width">
-        <ul className={'tech-stack stack'}>
+        <ul className={"tech-stack stack"}>
           <li>React JS</li>
           <li>Gatsby</li>
           <li>JS</li>
@@ -24,14 +25,14 @@ const Skills = () => (
         </ul>
       </div>
     </section>
-  
+    
     {/*// S O F T W A R E */}
     <section className="skill">
       <aside className="sidebar-left">
         <h3 className="software">Software</h3>
       </aside>
       <div className="container-full-width">
-        <ul className={'software-stack stack'}>
+        <ul className={"software-stack stack"}>
           <li>Jetbrains Webstorm IDE</li>
           <li>Sublime Text</li>
           <li>Jira</li>
@@ -42,7 +43,7 @@ const Skills = () => (
         </ul>
       </div>
     </section>
-    
+  
   </div>
 )
 
