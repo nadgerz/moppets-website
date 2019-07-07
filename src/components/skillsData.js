061f11bc5580a5da92bb651f0bede27ce1ca84fd
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-07-07 21:41:58 +0200

    created skillsData.js

---
+++ src/components/skillsData.js
@@ -0,0 +1,86 @@
+import React from "react"
+
+let counter = 1;
+
+export const tech = [
+  {
+    id: `tech-${counter}`,
+    name: 'React',
+    skillLevel: 3,
+  },
+  {
+    id: "tech-2",
+    name: "Gatsby",
+    skillLevel: 3
+  },
+  {
+    id: "tech-3",
+    name: "JS",
+    skillLevel: 4,
+  },
+  {
+    id: "tech-4",
+    name: "Gatsby",
+    skillLevel: 3
+  },
+  {
+    id: "tech-5",
+    name: "JS",
+    skillLevel: 3
+  },
+  {
+    id: "tech-6",
+    name: "SASS",
+    skillLevel: 3
+  },
+  {
+    id: "tech-7",
+    name: "SASS",
+    skillLevel: 3
+  },
+  {
+    id: "tech-8",
+    name: "CSS",
+    skillLevel: 3
+  },
+  {
+    id: "tech-9",
+    name: "Flexbox",
+    skillLevel: 3
+  },
+  {
+    id: "tech-10",
+    name: "HTML",
+    skillLevel: 3
+  },
+  {
+    id: "tech-11",
+    name: "Drupal",
+    skillLevel: 3
+  },
+  {
+    id: "tech-12",
+    name: "Git",
+    skillLevel: 3
+  },
+]
+
+
+export const software = [
+  {
+    id: "software-1",
+    title: "Freelance Front End Developer and Designer",
+    description:
+      <ul>
+        <li>React JS</li>
+        <li>Gatsby</li>
+        <li>JS</li>
+        <li>SASS</li>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>Git</li>
+        <li>Drupal</li>
+      </ul>,
+  },
+
+]
