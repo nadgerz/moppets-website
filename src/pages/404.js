d8e926e122f2c84813e8beda2b4d3dd5988e8057
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-07-08 16:36:10 +0200

    importing 404.scss into 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,23 @@
-import React from "react"
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import banana from '../assets/images/banana.png'
+import "../assets/css/pages/404.scss"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Not found"/>
+    <div className={'not-found-page'}>
+      <div className={'text-404'}>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
+      
+      <div className={'container-third-width image-container'}>
+        <img className={'imgBanana'} src={banana} alt="scared banana"/>
+      </div>
+    </div>
   </Layout>
 )
 
